63c7138dda9acba49c5baafe0f41f8c4fc0f2232
Lauda Carly <lauda@nel.re>
2025-12-20 13:45:34 +0100

    Add actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
 
-fn main() {
-    // Initialize .env environment variables
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    println!("test");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
