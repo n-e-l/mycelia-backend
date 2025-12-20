efd4e62393a14bc57e27c0878e2967d966261a71
Lauda Carly <lauda@nel.re>
2025-12-20 13:50:30 +0100

    Use /api

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(web::scope("/api").route("/hey", web::get().to(manual_hello)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
